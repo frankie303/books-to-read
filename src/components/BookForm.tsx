df26afe5daade8b939bbe041b6b2502a201ff397
mert <uzeyirmertciflikli@gmail.com>
2020-12-27 11:38:54 +0100

    add BookForm

---
+++ src/components/BookForm.tsx
@@ -0,0 +1,38 @@
+import React, { useContext, useState } from 'react';
+import { BookContext } from '../contexts/BookContext';
+
+export const NewBookForm = () => {
+  const { addBook } = useContext(BookContext);
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    addBook(title, author);
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='book title'
+        value={title}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setTitle(e.target.value)
+        }
+        required
+      />
+      <input
+        type='text'
+        placeholder='author'
+        value={author}
+        onChange={e => setAuthor(e.target.value)}
+        required
+      />
+      <input type='submit' value='add book' />
+    </form>
+  );
+};
--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,11 +1,10 @@
-import { read } from 'fs';
 import React, { useContext } from 'react';
 import { BookContext } from '../contexts/BookContext';
 import { BookDetails } from './BookDetails';
 
 export const BookList = () => {
   const { books } = useContext(BookContext);
-  useContext(BookContext);
+
   return books.length ? (
     <div className='book-list'>
       <ul>
